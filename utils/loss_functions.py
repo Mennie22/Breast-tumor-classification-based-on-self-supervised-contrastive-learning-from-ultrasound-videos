1fffb5591315320d1f230f87e8dc68e0f5d6bea7
Mennie22 <143776934+Mennie22@users.noreply.github.com>
2023-11-30 16:11:39 +0800

    Delete utils directory

--- utils/loss_functions.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-def LOSS(args):
-    if args.mode== 'train':
-        return nn.MSELoss(reduction='sum')
-    elif args.mode == 'test':
-        return nn.MSELoss(reduction='sum')
-    else:
-        raise IndexError
